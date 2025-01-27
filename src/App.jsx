1d6bbc479d38fe067b351c3a4771e5e29cc1abbc
amalaguti <adrianma78@hotmail.com>
2025-01-27 17:53:06 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
 import Header from "./components/layout/header/Header.jsx";
 import Navbar from "./components/layout/navbar/Navbar.jsx";
 import { Component as Footer } from "./components/layout/footer/Footer.jsx";
@@ -6,12 +8,18 @@ import ItemListContainer from "./components/pages/itemListContainer/ItemListCont
 function App() {
   return (
     <>
-      <Header user="Adrian" />
-      <Navbar platform="Mock" />
-      <main>
-        <ItemListContainer platform="Mock" />
-      </main>
-      <Footer />
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Header user="Adrian" />
+            <Navbar platform="Mock" />
+            <main>
+            <ItemListContainer platform="Mock" />
+            </main>
+            <Footer />
+          </>
+        } />
+      </Routes>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { BrowserRouter, Routes, Route } from "react-router";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      {/* <App /> */}
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
