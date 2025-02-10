83de92c3dc09a91003a639ee2fef5a8903335cce
Adrian Malaguti <adrianma78@hotmail.com>
2025-02-09 21:11:14 -0300

    cart total

--- src/components/common/cartWidget/CartWidget.jsx
+++ src/components/common/cartWidget/CartWidget.jsx
@@ -1,11 +1,20 @@
 import "./cartWidget.css";
 import { PiSolarPanelFill } from "react-icons/pi";
 import { Link } from "react-router";
+import { useContext } from "react";
+import { CartContext } from "../../../context/CartCreateContext";
+
 export const CartWidget = () => {
+  const { getTotalItems } = useContext(CartContext);
+  let total = getTotalItems();
+
   return (
     <div>
       <Link to="/cart">
         <PiSolarPanelFill id="cartLogo" />
+        <div>
+          <h2>TOTAL {total}</h2>
+        </div>
       </Link>
     </div>
   );
