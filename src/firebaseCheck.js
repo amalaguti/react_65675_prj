52e55d5e63f9e7bcdf81c4444a7dc9c2891dea91
Adrian Malaguti <adrianma78@hotmail.com>
2025-02-17 11:42:35 -0300

    firebaseCheck

--- src/firebaseCheck.js
+++ src/firebaseCheck.js
@@ -1,8 +1,38 @@
 import { db } from './firebaseConfig.js';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, query, where } from "firebase/firestore";
 
-const querySnapshot = await getDocs(collection(db, "notifications"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
-});
+export const getAllNotifications = async () => {
+  const querySnapshot = await getDocs(collection(db, "notifications"));
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+  });
+}
+
+// getAllNotifications()
+
+
+export const queryByStatus = async (status) => {
+  let notificationsCollection = collection(db, "notifications");
+  let consulta = notificationsCollection;
+  
+  console.log("status:", status);
+  let porcionDeLaColeccion = query(
+    notificationsCollection,
+    where("status", "==", status)
+  );
+  consulta = porcionDeLaColeccion;
+  
+  const getProducts = getDocs(consulta);
+  return getProducts;
+}
+
+queryByStatus("new")
+  .then((res) => {
+    const array = res.docs.map((elemento) => {
+      console.log(`${elemento.id} => ${JSON.stringify(elemento.data())}`);
+    });
+    
+  })
+  .catch((error) => console.log(error));
+
 
