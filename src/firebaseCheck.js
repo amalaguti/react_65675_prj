9557bd4f3bd784d70a2667269f5c2d84d2872209
Adrian Malaguti <adrianma78@hotmail.com>
2025-02-17 10:36:49 -0300

    dataLoad

--- src/firebaseCheck.js
+++ src/firebaseCheck.js
@@ -1,5 +1,5 @@
 import { db } from './firebaseConfig.js';
-import { collection, doc, getDocs } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 
 const querySnapshot = await getDocs(collection(db, "notifications"));
 querySnapshot.forEach((doc) => {
--- src/firebaseDataLoad.js
+++ src/firebaseDataLoad.js
@@ -1,3 +1,16 @@
 import { mockNotifications } from "./mock/notifications/notifications.js";
+import { db } from './firebaseConfig.js';
+import { collection, addDoc, getDocs, query, where } from "firebase/firestore";
 
-console.log(mockNotifications);
+
+const dataLoad = () => {
+    let notificationsCollection = collection(db, "notifications");
+
+    mockNotifications.forEach((elemento) => {
+        console.log(elemento);
+        addDoc(notificationsCollection, elemento);
+    });
+}
+
+
+dataLoad()
