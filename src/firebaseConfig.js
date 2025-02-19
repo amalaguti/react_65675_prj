1061bf6663db4c8b0a6e5e396e7b9cd5d4d60bdd
Adrian Malaguti <adrianma78@hotmail.com>
2025-02-19 14:48:47 -0300

    counters in navbar

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,18 +6,20 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAotXh-pq6Wnspb9eF9r76klccD10aBU70",
-    authDomain: "notifmgr-65675.firebaseapp.com",
-    projectId: "notifmgr-65675",
-    storageBucket: "notifmgr-65675.firebasestorage.app",
-    messagingSenderId: "205291755703",
-    appId: "1:205291755703:web:a7d5d30db3ac301c43d532",
-    measurementId: "G-6R77VNYFRE"
-  };
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID
+};
 
 // Initialize Firebase
+console.log(firebaseConfig)
 const app = initializeApp(firebaseConfig);
 console.log('Firebase initialized');
+
 
 // Export db
 export const db = getFirestore(app);
